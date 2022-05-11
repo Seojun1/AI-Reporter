c0042108e48c18acf3efb1478d6767fc599efe74
LeeSeoJun <SeoCoding123@gmail.com>
2022-05-11 21:58:23 +0900

    commit

--- Quiz/03.py
+++ Quiz/03.py
@@ -1,8 +1,5 @@
-# 하나의 문장만을 위한 도전문제3번 ( 하나의 문장만 해당되기 때문에 효율성이 낮다 )
-
 import tensorflow as tf
 import pandas as pd
-import numpy as np
 
 list1 = []
 data = pd.read_csv('../data/titles.csv')
@@ -11,25 +8,16 @@ titles = data['title'].values
 # preprocessing -> 전처리 단계 (학습하기 전에 처리하는 단계) / 데이터 가공 과정
 tokenizer = tf.keras.preprocessing.text.Tokenizer()
 tokenizer.fit_on_texts(titles)
-# print(tokenizer.word_index)
-
-# sequence -> 수열 / 어떤 수의 나열로 바꿔서 표현해주는 것
-sequence = tokenizer.texts_to_sequences([titles[0]])[0]
-# print(sequence)
 
 x = []
-xx = []
+y = []
+for i in range(len(titles)):
+    # sequence -> 수열 / 어떤 수의 나열로 바꿔서 표현해주는 것
+    sequence = tokenizer.texts_to_sequences([titles[i]])[0]
+    for j in range(1, len(sequence)):
+        x.append(sequence[:j])
+        y.append(sequence[j])
 
-for i in range(len(sequence)):
-    b = np.array(
-        [
-            x.append(sequence[i])
-        ]
-    )
-    # 배열 자료형 --> 배열을 복사할때 append를 사용하면 참조복사(최종 결괏값만 복사)를 하기 때문에 발생한 문제를 copy로 사용하면 해결되는 문제였다.
-    xx.append(x.copy())
-print('x값 : ', xx)
+print(x) # x의 값
+print(y) # y의 값
 
-# y 값
-y = sequence[1:]
-print('y값 : ', y)
---
+++ lab_data/08. to_categorical.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+
+data = [0, 1, 2, 3]
+categorical_data = tf.keras.utils.to_categorical(data, num_classes=4)
+print(categorical_data)
