da9803aae4bc7e02d382187ce76bd19710f72d8a
LeeSeoJun <SeoCoding123@gmail.com>
2022-05-30 21:55:06 +0900

    jdksfljas;lkgjasdkl

--- lab_ai/07. predict.py
+++ lab_ai/07. predict.py
@@ -3,7 +3,7 @@ import numpy as np
 
 model = tf.keras.models.load_model('../models/Softmax.h5')
 
-predict = model.predict([[0, 1, 2], [1, 1, 1]])
+predict = model.predict([[0, 1, 2]])
 print(predict)
 
 index_word = ['가', '나', '다', '라', '마', '바']
